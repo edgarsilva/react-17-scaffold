1dd85ebdc8e8525b9d4ef5c10047d341c49eda86
edgarsilva <edgar.osc@gmail.com>
2021-04-26 15:46:53 -0500

    Heroku setup

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`React 17 Scaffold App listening on PORT: ${PORT}`);
 })
